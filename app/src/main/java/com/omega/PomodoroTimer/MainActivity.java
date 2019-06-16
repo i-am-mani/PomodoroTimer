ed4125ee9d82bebcfb4e42688a628236954f5fcc
Mandeep <mandeepch868@gmail.com>
2019-06-16 20:37:02 +0530

    Add ServiceConnection for TimerService

--- app/src/main/java/com/omega/PomodoroTimer/MainActivity.java
+++ app/src/main/java/com/omega/PomodoroTimer/MainActivity.java
@@ -2,9 +2,17 @@ package com.omega.PomodoroTimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+
+import com.omega.PomodoroTimer.Services.TimerService;
 
 public class MainActivity extends AppCompatActivity {
+
+    ServiceConnection mServiceConnection = new TimerServiceConnection();
+    TimerService mTimerService = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Create a foreground service with IBINDER,
+
+    private class TimerServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            TimerService.ServiceBinder timerService = (TimerService.ServiceBinder) service;
+            mTimerService = timerService.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mTimerService = null;
+        }
+    }
 }
