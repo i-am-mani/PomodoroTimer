7c58e06f31dd9bcc8248b55f5169324cc898f1d2
Mandeep <mandeepch868@gmail.com>
2019-06-17 15:09:05 +0530

    Bind Service

--- app/src/main/java/com/omega/PomodoroTimer/MainActivity.java
+++ app/src/main/java/com/omega/PomodoroTimer/MainActivity.java
@@ -2,12 +2,16 @@ package com.omega.PomodoroTimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 
 import com.omega.PomodoroTimer.Services.TimerService;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, TimerService.class);
+        bindService(intent,mServiceConnection, Service.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unbindService(mServiceConnection);
+        mTimerService = null;
+    }
 
     private class TimerServiceConnection implements ServiceConnection {
         @Override
